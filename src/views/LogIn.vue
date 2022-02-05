4830df5f829d51a9c395bd987bc42b768d7225d9
Wannachat Surasiang <robotics.za@gmail.com>
2022-02-05 23:58:39 +0700

    add authentication flow

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,13 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>LogIn Page</h1>
+  <q-page>
+    <div class="row flex-center">
+      <h1>LogIn Page</h1>
+    </div>
+    <div class="row flex-center">
+      <q-btn flat class="q-mr-xs" @click="signup">sign up</q-btn>
+    </div>
+    
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'LogIn',
+  setup () {
+    const router = useRouter();
+    const signup = () => {
+      router.push({name: 'SignUp'})
+    }
+    return {
+      signup
+    }
+  }
 });
 </script>
