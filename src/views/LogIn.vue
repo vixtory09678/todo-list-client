7677e70de81b1809f842e1cb8ebf137f75c452aa
Wannachat Surasiang <robotics.za@gmail.com>
2022-02-06 06:36:23 +0700

    implement code handle when auth api return error

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -6,6 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 import LogInForm from '@/components/LogInForm.vue'
 import { AuthApi } from '../api'
 
@@ -15,12 +16,14 @@ export default defineComponent({
     LogInForm
   },
   setup () {
-    const formSubmit = async (username: string, password: string) => {
-      const res = await AuthApi.login(username, password);
-      if (res) {
-        localStorage.setItem('accessToken', res.accessToken);
+    const router = useRouter()
+    const formSubmit = async (form: {username: string, password: string}) => {
+      const res = await AuthApi.login(form.username, form.password);
+      if (res.loginResponse) {
+        localStorage.setItem('accessToken', res.loginResponse.accessToken);
+        router.push({name:'Home'})
       } else {
-        console.log('login fail')
+        alert(res.err)
       }
     }
 
