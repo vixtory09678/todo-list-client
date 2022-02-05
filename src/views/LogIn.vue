0e40decdd5d572e4db98cbec0b54c3191f58a4e6
Wannachat Surasiang <wannachat.sur@wipay.co.th>
2022-02-05 23:06:00 +0700

    first init

---
+++ src/views/LogIn.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>LogIn Page</h1>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LogIn',
+});
+</script>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>SignUp Page</h1>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SignUp',
+});
+</script>
