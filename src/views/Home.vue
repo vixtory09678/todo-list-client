26c654b780382b10901086beb150e1a1839602ac
Wannachat Surasiang <robotics.za@gmail.com>
2022-02-06 08:02:24 +0700

    add todo list in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <q-page>
-    <h1>Home Page</h1>
+    <ToDoList />
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ToDoList from '@/components/ToDoList.vue'
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    ToDoList
+  }
 });
 </script>
