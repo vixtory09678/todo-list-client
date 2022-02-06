478b69318eaf969784e00aaf451227ed307d8c75
Wannachat Surasiang <robotics.za@gmail.com>
2022-02-06 19:41:30 +0700

    change to use global constant variable

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,11 +1,10 @@
-import axios, { AxiosError } from 'axios'
+import axios from 'axios'
 import { LogInResponse, SignUpResponse } from './interfaces/auth-res.interface'
-
-const host = process.env.TODO_SERVICE || 'http://localhost:3000'
+import { HOST } from './constant'
 
 const signup = async function(username: string, password: string) : Promise<{signupResponse?: SignUpResponse, err?:string}> {
   try {
-    const resp = await axios.post(host + '/auth/signup', { username, password: btoa(password) })
+    const resp = await axios.post(HOST + '/auth/signup', { username, password: btoa(password) })
     const signupResponse: SignUpResponse = resp.data
     return { signupResponse, err: undefined }
   } catch (err) {
@@ -18,7 +17,7 @@ const signup = async function(username: string, password: string) : Promise<{sig
 
 const login = async function(username: string, password: string) : Promise<{loginResponse?: LogInResponse, err?:string}> {
   try {
-    const resp = await axios.post(host + '/auth/login', { username, password: btoa(password) })
+    const resp = await axios.post(HOST + '/auth/login', { username, password: btoa(password) })
     const loginResponse: LogInResponse = resp.data
     return { loginResponse, err: undefined }
   } catch (err) {
