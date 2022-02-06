2498e10274242f75912203dddcd2a36df61e6e5c
Wannachat Surasiang <robotics.za@gmail.com>
2022-02-06 08:02:04 +0700

    add todo code base

---
+++ src/components/ToDoItemAdd.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-card style="width: 700px; max-width: 80vw;">
+    <q-card-section>
+      <div class="text-h6">Medium</div>
+    </q-card-section>
+
+    <q-card-section class="q-pt-none">
+      Click/Tap on the backdrop.
+    </q-card-section>
+
+    <q-card-actions align="right" class="bg-white text-teal">
+      <q-btn flat label="OK" v-close-popup />
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ToDoItemAdd',
+});
+</script>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="row q-mt-xl justify-center">
+    <q-card bordered class="my-card">
+      <q-card-section >
+        <div class="row items-center">
+          <div class="col text-h6">
+            ToDo List
+          </div>
+          <div class="col-auto">
+            <q-btn color="green" icon="add" @click="addToDoItem"></q-btn>
+          </div>
+        </div>
+      </q-card-section>
+      <q-separator inset />
+    </q-card>
+
+    <q-dialog v-model="isShowAddDialog">
+      <ToDoItemAdd/>
+    </q-dialog>
+
+    <q-dialog v-model="isShowEditDialog">
+      <ToDoItemEdit/>
+    </q-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import ToDoItemAdd from '@/components/ToDoItemAdd.vue';
+import ToDoItemEdit from '@/components/ToDoItemEdit.vue';
+
+export default defineComponent({
+  name: 'ToDoList',
+  components: {
+    ToDoItemAdd,
+    ToDoItemEdit
+  },
+  setup () {
+    let isShowAddDialog = ref(false)
+    let isShowEditDialog = ref(false)
+
+    const addToDoItem = () => {
+      showDialogAdd()
+    }
+
+    const showDialogAdd = () => {
+      isShowAddDialog.value = true
+    }
+
+    return {
+      isShowAddDialog,
+      isShowEditDialog,
+      addToDoItem
+    }
+  }
+});
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 50%
+</style>
