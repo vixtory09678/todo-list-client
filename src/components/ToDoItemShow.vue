541cb9036addbad875ff2c98be83b4d7ca23f340
Wannachat Surasiang <robotics.za@gmail.com>
2022-02-07 16:32:20 +0700

    add ToDoItemShow component for displat todo item when user selected item

---
+++ src/components/ToDoItemShow.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-card flat class="my-card">
+    <q-card-section>
+      <div v-if="todoItem.picturePath">
+        <q-img
+          src="https://cdn.quasar.dev/img/parallax1.jpg"
+          style="height: 200px; padding">
+          <div class="absolute-bottom text-h6">
+            {{todoItem.name}}
+          </div>
+        </q-img>
+
+        <q-btn
+          @click="edit"
+          fab
+          color="primary"
+          icon="edit"
+          class="absolute"
+          style="top: 0; right: 20px; transform: translateY(50%);"
+        />
+      </div>
+      <div v-else>
+        <div class="row no-wrap items-center q-pa-md">
+          <div class="col-8">
+            <div class="text-h5">{{todoItem.name}}</div>
+          </div>
+
+          <div class="col text-grey text-caption q-ml-md">
+            <q-card-actions align="right">
+              <q-btn
+                @click="edit"
+                fab
+                color="primary"
+                icon="edit"
+                class="absolute"
+              />
+            </q-card-actions>
+            <div v-if="hasFile==true" style="margin-left: 15px">
+              asdfasdf
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </q-card-section>
+
+    <div v-if="todoItem.detail">
+      <q-separator inset style="margin-bottom: 10px"/>
+      <q-card-section>
+        <div class="text-subtitle2">{{todoItem.detail}}</div>
+      </q-card-section>
+    </div>
+    
+
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { GetToDoList } from '../api/interfaces/todo-res.interface';
+
+export default defineComponent({
+  name: 'ToDoItemShow',
+  props:{
+    item: Object as PropType<GetToDoList>
+  },
+  setup (props, { emit }) {
+    const todoItem = ref(props.item);
+
+    const edit = () => {
+      emit('onItemNeedToEdit', todoItem.value)
+    }
+
+    return { 
+      todoItem,
+      edit
+    }
+  }
+});
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 600px
+</style>
