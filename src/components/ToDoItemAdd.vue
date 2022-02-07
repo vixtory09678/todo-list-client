ade080e0b3a7b48c0d0084f1d1ac78ba1d80e3b2
Wannachat Surasiang <robotics.za@gmail.com>
2022-02-08 02:08:40 +0700

    add image upload path

--- src/components/ToDoItemAdd.vue
+++ src/components/ToDoItemAdd.vue
@@ -1,8 +1,19 @@
 <template>
   <q-card style="width: 700px; max-width: 80vw;">
     <q-card-section>
-      <div class="row no-wrap items-center">
 
+      <q-uploader
+        flat
+        style="width: 100%"
+        label="Upload your cover image"
+        accept=".jpg, image/*"
+        hide-upload-btn
+        max-files="1"
+        @added="onFileAdded"
+        @removed="onFileRemoved"
+      />
+
+      <div class="row no-wrap items-center">
         <div class="col-8">
           <q-input bottom-slots v-model="todoName" label="Todo Name">
             <template v-slot:before>
@@ -15,9 +26,8 @@
         </div>
 
         <div class="col text-grey text-caption q-ml-md">
-          <q-card-actions>
-            <q-btn flat color="red" icon="image">upload</q-btn>
-            <q-btn flat color="positive" icon="save" @click="save">save</q-btn>
+          <q-card-actions align="right">
+            <q-btn color="positive" icon="save" @click="save">save</q-btn>
           </q-card-actions>
           <div v-if="hasFile==true" style="margin-left: 15px">
             asdfasdf
@@ -40,20 +50,33 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, reactive, ref } from 'vue';
 
 export default defineComponent({
   name: 'ToDoItemAdd',
   setup (props, { emit }) {
-    const hasFile = ref('false')
+    let picture: {hasFile: boolean, files: Array<File>} = reactive({hasFile: false, files: []})
+    const hasFile = ref(false)
     const todoName = ref('')
     const detail = ref('')
 
+    const onFileAdded  = (inPutFiles: File[]) => {
+      picture.hasFile = true
+      picture.files = [...inPutFiles]
+    }
+
+    const onFileRemoved = (inPutFiles: File[]) => {
+      picture.hasFile = true
+      picture.files = []
+    }
+
     const save = () => {
+
       if (todoName.value) {
         emit('onAddTodoItem', {
           name: todoName.value,
-          detail: detail.value
+          detail: detail.value,
+          file: picture.hasFile ? picture.files[0] : undefined
         })
       } else {
         alert('Name is empty')
@@ -64,7 +87,9 @@ export default defineComponent({
       todoName,
       detail,
       hasFile,
-      save
+      save,
+      onFileAdded,
+      onFileRemoved
     }
   }
 });
