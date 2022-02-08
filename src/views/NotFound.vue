4180734461b879577a822e83b0701adf9cacb08d
Wannachat Surasiang <robotics.za@gmail.com>
2022-02-08 09:30:33 +0700

    add file not found

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <h1>Page Not Found</h1>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NotFound',
+});
+</script>
